ae097d58c83e74b74bd9eaca0321ebad4ca552e9
Isabella-Slaby <isabella.slaby@gmail.com>
2024-08-30 12:17:17 -0400

    Add tailwind.config.js file for CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this path based on your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        christianaBlue: '#003366',
+        christianaGreen: '#006633',
+        christianaTeal: '#009999',
+      },
+    },
+  },
+  plugins: [],
+}
