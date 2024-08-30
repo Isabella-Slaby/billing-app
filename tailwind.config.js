e3cce24c0cfbd8280713c146fb77c8e55af83646
Isabella-Slaby <isabella.slaby@gmail.com>
2024-08-30 15:30:47 -0400

    added safelist to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}', // Adjust this path based on your project structure
     './public/index.html',
   ],
+  safelist: [
+    'transition-opacity',
+    'duration-300',
+    'ease-in-out',
+    'opacity-100',
+    'opacity-0',
+  ],
   theme: {
     extend: {
       colors: {
