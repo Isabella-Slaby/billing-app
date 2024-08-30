ab4fd193bf65d5538a698c7e7feca11cced09925
Isabella-Slaby <isabella.slaby@gmail.com>
2024-08-30 12:34:23 -0400

    added header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-christianaBlue p-4 text-white">
+      <h1 className="text-4xl font-bold">Welcome to the Billing App</h1>
+    </header>
+  );
+}
+
+export default Header;
